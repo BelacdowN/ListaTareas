1863d47c3cdc46e4d9da8282ab93e5a3a15ef374
BelacdowN <104211742+BelacdowN@users.noreply.github.com>
2022-08-27 15:40:40 -0300

    crear base de datos Json local y configurar

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/service/task.service';
+import { Task } from '../../task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks : Task[] = [];
+  
+  constructor(
+    private taskService: TaskService
+  ) { }
+
+  ngOnInit(): void {
+    //Como promesa
+    this.taskService.getTasks().subscribe( tasks =>[
+      this.tasks = tasks
+    ]);
+  }
+
+}
