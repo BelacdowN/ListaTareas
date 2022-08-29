1d4773c730bbe94165f5d5cfdbd353c4b7713466
BelacdowN <104211742+BelacdowN@users.noreply.github.com>
2022-08-29 11:45:07 -0300

    configurar borrado y touch de true y false de tasks

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Task } from 'src/app/task';
-import { TASKS } from 'src/app/mock-task';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../task';
+import { TASKS } from '../../mock-task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -10,10 +10,19 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task: Task = TASKS[0]
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter()
   constructor() { }
 
   faTimes = faTimes;
   ngOnInit(): void {
   }
-
+  onDelete(task:Task){
+    this.onDeleteTask.emit(task);
+  }
+  
+  onToggle(task:Task){
+    this.onToggleReminder.emit(task);
+  }
+  
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -11,14 +11,26 @@ export class TasksComponent implements OnInit {
   tasks : Task[] = [];
   
   constructor(
-    private taskService: TaskService
+    private taskService: TaskService 
   ) { }
 
   ngOnInit(): void {
     //Como promesa
-    this.taskService.getTasks().subscribe( tasks =>[
+    this.taskService.getTasks().subscribe( (tasks) =>[
       this.tasks = tasks
     ]);
   }
-
+  deleteTask(task:Task){
+    this.taskService.deleteTask(task)
+    .subscribe(
+      ()=>[
+      this.tasks = this.tasks.filter( (t) => {
+        return t.id !== task.id 
+      })
+    ])
+  }
+  toggleReminder(task:Task){
+    task.reminder = !task.reminder
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
 }
